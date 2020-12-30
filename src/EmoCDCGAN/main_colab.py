Critical fixes to remove bugs from gradient accumulation function. The name of models are also added in build functions.

src/EmoCDCGAN/main_colab.py
@@ -2,16 +2,16 @@ import os
 import numpy as np
 import tensorflow as tf
 
-from src.EmoCDCGAN.utils.data_preprocessing.data_packing import unpack_data_and_labels_npy
-from src.EmoCDCGAN.utils.data_preprocessing.load_utils import load_AffectNet_labels, load_image, crop_image, preprocess_image
 from src.EmoCDCGAN.resnet_blocks import create_generator_resnet_based, create_discriminator_resnet_based, \
     build_adversarial_model_resnet_based
+from src.EmoCDCGAN.utils.data_preprocessing.data_packing import unpack_data_and_labels_npy
+from src.EmoCDCGAN.utils.data_preprocessing.load_utils import preprocess_image
 from src.EmoCDCGAN.utils.train_utils import train_n_mini_batches
 from src.EmoCDCGAN.utils.vizualization_utils import visualize_images
 
 
 def train():
-    path_to_data='E:\\Databases\\AffectNet\\AffectNet\\Batches'
+    path_to_data='/content/drive/MyDrive/Batches'
 
     # params
     latent_space_shape=200
@@ -19,7 +19,7 @@ def train():
     image_size=224
     batch_size=int(64)
     train_steps=40000
-    validate_each_step=50
+    validate_each_step=25
 
     # data for validation generator
     noise_validation=np.random.uniform(-1., 1., (20, latent_space_shape,1))
@@ -39,15 +39,15 @@ def train():
     # discriminator model
     input_x_disc=tf.keras.layers.Input((image_size, image_size, 3))
     discriminator_model=create_discriminator_resnet_based(input_x_disc, input_y, image_size)
-    #optimizer_disc=tf.keras.optimizers.RMSprop(lr=0.0002, decay=6e-8)
-    optimizer_disc = tf.keras.optimizers.Adam(lr=0.0002, amsgrad=True)
+    optimizer_disc=tf.keras.optimizers.RMSprop(lr=0.0001, decay=6e-8)
+    #optimizer_disc = tf.keras.optimizers.Adam(lr=0.0001, amsgrad=True)
     discriminator_model.compile(optimizer=optimizer_disc, loss='binary_crossentropy')
 
     # adversarial model
     discriminator_model.trainable=False
     adversarial_model=build_adversarial_model_resnet_based(generator_model, discriminator_model, input_x_gen, input_y)
-    #optimizer_adv=tf.keras.optimizers.RMSprop(lr=0.0002*0.5, decay=6e-8*0.5)
-    optimizer_adv=tf.keras.optimizers.Adam(lr=0.0001, amsgrad=True)
+    optimizer_adv=tf.keras.optimizers.RMSprop(lr=0.0001, decay=6e-8)
+    #optimizer_adv=tf.keras.optimizers.Adam(lr=0.0002, amsgrad=True)
     adversarial_model.compile(optimizer=optimizer_adv, loss='binary_crossentropy')
 
     # summaries
@@ -89,8 +89,8 @@ def train():
         descriminator_loss=train_n_mini_batches(model=discriminator_model,
                                                 data=[train_discriminator_batch_images, train_discriminator_batch_labels],
                                                 labels=y_discriminator,
-                                                num_mini_batches=int(y_discriminator.shape[0]/2),
-                                                batch_size=2, loss=tf.keras.losses.binary_crossentropy)
+                                                num_mini_batches=int(y_discriminator.shape[0]),
+                                                batch_size=8, loss=tf.keras.losses.binary_crossentropy)
         #descriminator_loss=discriminator_model.train_on_batch([train_discriminator_batch_images, train_discriminator_batch_labels],y_discriminator)
 
         # train generator
@@ -105,8 +105,8 @@ def train():
                                                   data=[z,
                                                         fake_labels],
                                                   labels=y_adversarial_network,
-                                                  num_mini_batches=int(y_adversarial_network.shape[0] / 2),
-                                                  batch_size=2, loss=tf.keras.losses.binary_crossentropy)
+                                                  num_mini_batches=int(y_adversarial_network.shape[0]),
+                                                  batch_size=8, loss=tf.keras.losses.binary_crossentropy)
 
         #adversarial_loss = adversarial_model.train_on_batch([z, fake_labels], y_adversarial_network)
 

src/EmoCDCGAN/resnet_blocks.py
@@ -112,14 +112,14 @@ def create_discriminator_resnet_based(x_input, y_input, image_shape, group_norm_
     #x = resnet_identity(input=x, num_filters=(64, 256), group_norm_groups=group_norm_groups)
 
     x = resnet_conv_block(input=x, num_filters=(128, 512), stride=(2, 2), group_norm_groups=group_norm_groups)
-    #x = resnet_identity(input=x, num_filters=(128, 512), group_norm_groups=group_norm_groups)
+    x = resnet_identity(input=x, num_filters=(128, 512), group_norm_groups=group_norm_groups)
     #x = resnet_identity(input=x, num_filters=(128, 512), group_norm_groups=group_norm_groups)
 
     x = resnet_conv_block(input=x, num_filters=(256, 1024), stride=(2, 2), group_norm_groups=group_norm_groups)
-    #x = resnet_identity(input=x, num_filters=(256, 1024), group_norm_groups=group_norm_groups)
+    x = resnet_identity(input=x, num_filters=(256, 1024), group_norm_groups=group_norm_groups)
     #x = resnet_identity(input=x, num_filters=(256, 1024), group_norm_groups=group_norm_groups)
 
-    #x = resnet_conv_block(input=x, num_filters=(512, 1024), stride=(2, 2), group_norm_groups=group_norm_groups)
+    x = resnet_conv_block(input=x, num_filters=(512, 1024), stride=(2, 2), group_norm_groups=group_norm_groups)
     #x = resnet_identity(input=x, num_filters=(512, 1024), group_norm_groups=group_norm_groups)
 
     x = tf.keras.layers.Conv2D(filters=64, kernel_size=7, activation=None, strides=(2, 2), padding='same')(x)
@@ -129,11 +129,11 @@ def create_discriminator_resnet_based(x_input, y_input, image_shape, group_norm_
     x=tf.keras.layers.GlobalAveragePooling2D()(x)
     x=tf.keras.layers.Dense(512, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.0001))(x)
     x=tf.keras.layers.Dense(1, activation='sigmoid')(x)
-    model=tf.keras.Model(inputs=[x_input, y_input], outputs=x)
+    model=tf.keras.Model(inputs=[x_input, y_input], outputs=x, name='discriminator')
     tf.keras.utils.plot_model(model, show_shapes=True, to_file='discriminator_resnet_based_model.png')
     return model
 
-def create_generator_resnet_based(input_x, input_y, discriminator_output_map_shape=(14,14,64), group_norm_groups=32):
+def create_generator_resnet_based(input_x, input_y, discriminator_output_map_shape=(7,7,64), group_norm_groups=32):
     concat=tf.keras.layers.concatenate([input_x, input_y])
     x=tf.keras.layers.Flatten()(concat)
     x=tf.keras.layers.Dense(discriminator_output_map_shape[0]*
@@ -148,14 +148,14 @@ def create_generator_resnet_based(input_x, input_y, discriminator_output_map_sha
     x = resnet_identity_transponse(input=x, num_filters=(64, 256), group_norm_groups=group_norm_groups)
 
     x = resnet_conv_transpose(input=x, num_filters=(128, 512), stride=(2, 2), group_norm_groups=group_norm_groups)
-    #x = resnet_identity_transponse(input=x, num_filters=(128, 512), group_norm_groups=group_norm_groups)
+    x = resnet_identity_transponse(input=x, num_filters=(128, 512), group_norm_groups=group_norm_groups)
     #x = resnet_identity_transponse(input=x, num_filters=(128, 512), group_norm_groups=group_norm_groups)
 
     x = resnet_conv_transpose(input=x, num_filters=(256, 1024), stride=(2, 2), group_norm_groups=group_norm_groups)
-    #x = resnet_identity_transponse(input=x, num_filters=(256, 1024), group_norm_groups=group_norm_groups)
+    x = resnet_identity_transponse(input=x, num_filters=(256, 1024), group_norm_groups=group_norm_groups)
     #x = resnet_identity_transponse(input=x, num_filters=(256, 1024), group_norm_groups=group_norm_groups)
 
-    #x = resnet_conv_transpose(input=x, num_filters=(512, 1024), stride=(2, 2), group_norm_groups=group_norm_groups)
+    x = resnet_conv_transpose(input=x, num_filters=(512, 1024), stride=(2, 2), group_norm_groups=group_norm_groups)
     #x = resnet_identity_transponse(input=x, num_filters=(512, 1024), group_norm_groups=group_norm_groups)
 
     x = tf.keras.layers.Conv2DTranspose(filters=64, kernel_size=7, activation=None, strides=(2, 2), padding='same')(x)
@@ -170,7 +170,7 @@ def create_generator_resnet_based(input_x, input_y, discriminator_output_map_sha
     return model
 
 def build_adversarial_model_resnet_based(generator, discriminator, latent_space_input, labels_input):
-    model=tf.keras.Model(inputs=[latent_space_input, labels_input], outputs=[discriminator([generator([latent_space_input, labels_input]), labels_input])])
+    model=tf.keras.Model(inputs=[latent_space_input, labels_input], outputs=[discriminator([generator([latent_space_input, labels_input]), labels_input])], name='adversarial')
     return model
 
 if __name__ == "__main__":

src/EmoCDCGAN/utils/train_utils.py
@@ -3,52 +3,67 @@ import numpy as np
 from sklearn.metrics import accuracy_score
 
 
-def train_n_mini_batches(model, data, labels, num_mini_batches, batch_size=4, loss=tf.keras.losses.categorical_crossentropy):
+def train_n_mini_batches(model, data, labels, num_mini_batches, batch_size=4,
+                         loss=tf.keras.losses.categorical_crossentropy):
     tf.executing_eagerly()
-    train_vars=model.trainable_variables
-    accum_gradient = [tf.zeros_like(this_var) for this_var in train_vars]
-    total_loss=0
-    for i in range(num_mini_batches):
-        start_idx=i*batch_size
-        end_idx=(i+1)*batch_size
-        train_data=[data[0][start_idx:end_idx], data[1][start_idx:end_idx] ]
-        train_labels=labels[start_idx:end_idx].reshape((-1,1)).astype('float32')
-        with tf.GradientTape() as tape:
-            predictions=model(train_data)
-            loss_value = loss(train_labels, predictions)
-            #print('loss value:%f'%loss_value.numpy().mean())
-        total_loss+=loss_value.numpy().mean()
+    # print((model.name))
+    if model.name == 'discriminator':
+        model.trainable = True
+    # print(model.trainable)
+    train_vars = model.trainable_variables
 
-        gradients=tape.gradient(loss_value, train_vars)
-        accum_gradient=[(acum_grad + grad) for acum_grad, grad in zip(accum_gradient, gradients)]
+    # print('vars:',train_vars[0])
+    accum_gradient = [tf.zeros_like(this_var) for this_var in train_vars]
+    total_loss = 0
+    for i in range(num_mini_batches // batch_size):
+        start_idx = i * batch_size
+        end_idx = (i + 1) * batch_size
+        # print('start_idx:',start_idx)
+        # print('end_idx:',end_idx)
+        train_data = [data[0][start_idx:end_idx], data[1][start_idx:end_idx]]
+        train_labels = labels[start_idx:end_idx].reshape((-1, 1)).astype('float32')
+        with tf.GradientTape() as tape:
+            predictions = model(train_data, training=True)
+            # print('predictions:',predictions)
+            loss_value = loss(train_labels, predictions)
+            # print('loss value:%f'%loss_value.numpy().mean())
+        total_loss += loss_value.numpy().mean()
+
+        gradients = tape.gradient(loss_value, train_vars)
+        accum_gradient = [(acum_grad + grad) for acum_grad, grad in zip(accum_gradient, gradients)]
 
     accum_gradient = [this_grad / num_mini_batches for this_grad in accum_gradient]
 
     model.optimizer.apply_gradients(zip(accum_gradient, train_vars))
 
-    batch_loss = total_loss / num_mini_batches
+    batch_loss = total_loss / (num_mini_batches//batch_size)
+    if model.name == 'discriminator':
+        model.trainable = False
     return batch_loss
 
-def shuffle(x,y):
-    permut=np.random.permutation(x.shape[0])
+
+def shuffle(x, y):
+    permut = np.random.permutation(x.shape[0])
     return x[permut], y[permut]
+
 
 if __name__ == "__main__":
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-    x_train=x_train[..., np.newaxis].astype('float32')
-    x_train=x_train/255.
-    y_train=tf.keras.utils.to_categorical(y_train)
+    x_train = x_train[..., np.newaxis].astype('float32')
+    x_train = x_train / 255.
+    y_train = tf.keras.utils.to_categorical(y_train)
 
-    x_val=x_train[:250]
-    y_val=y_train[:250]
-    x_train=x_train[250:]
-    y_train=y_train[250:]
+    x_val = x_train[:250]
+    y_val = y_train[:250]
+    x_train = x_train[250:]
+    y_train = y_train[250:]
 
-    model=tf.keras.Sequential()
-    model.add(tf.keras.layers.Conv2D(32,4,strides=(1,1), activation='relu', padding='same', input_shape=x_train.shape[1:]))
-    model.add(tf.keras.layers.Conv2D(64,4,strides=(2,2), activation='relu', padding='same'))
-    model.add(tf.keras.layers.Conv2D(128,3,strides=(2,2), activation='relu', padding='same'))
-    model.add(tf.keras.layers.Conv2D(256,3,strides=(2,2), activation='relu', padding='same'))
+    model = tf.keras.Sequential()
+    model.add(
+        tf.keras.layers.Conv2D(32, 4, strides=(1, 1), activation='relu', padding='same', input_shape=x_train.shape[1:]))
+    model.add(tf.keras.layers.Conv2D(64, 4, strides=(2, 2), activation='relu', padding='same'))
+    model.add(tf.keras.layers.Conv2D(128, 3, strides=(2, 2), activation='relu', padding='same'))
+    model.add(tf.keras.layers.Conv2D(256, 3, strides=(2, 2), activation='relu', padding='same'))
     model.add(tf.keras.layers.GlobalAveragePooling2D())
     model.add(tf.keras.layers.Flatten())
     model.add(tf.keras.layers.Dense(10, activation='softmax'))
@@ -56,17 +71,18 @@ if __name__ == "__main__":
     model.summary()
 
     # val check
-    predictions=model.predict(x_val)
-    print('accuracy_score before train:%f'%accuracy_score(np.argmax(y_val, axis=-1), np.argmax(predictions, axis=-1)))
+    predictions = model.predict(x_val)
+    print('accuracy_score before train:%f' % accuracy_score(np.argmax(y_val, axis=-1), np.argmax(predictions, axis=-1)))
 
     for i in range(1000):
-        x_train, y_train=shuffle(x_train, y_train)
-        batch_loss=train_n_mini_batches(model=model, data=x_train[:32], labels=y_train[:32], num_mini_batches=8, batch_size=4,
-                         loss=tf.keras.losses.categorical_crossentropy)
-        #print('batch loss:%f'%batch_loss)
-        if i%25==0:
+        x_train, y_train = shuffle(x_train, y_train)
+        batch_loss = train_n_mini_batches(model=model, data=x_train[:32], labels=y_train[:32], num_mini_batches=8,
+                                          batch_size=4,
+                                          loss=tf.keras.losses.categorical_crossentropy)
+        # print('batch loss:%f'%batch_loss)
+        if i % 25 == 0:
             predictions = model.predict(x_val)
-            print('accuracy_score iteration %i train:%f' % (i,accuracy_score(np.argmax(y_val, axis=-1),
-                                                                    np.argmax(predictions, axis=-1))))
+            print('accuracy_score iteration %i train:%f' % (i, accuracy_score(np.argmax(y_val, axis=-1),
+                                                                              np.argmax(predictions, axis=-1))))
 
 